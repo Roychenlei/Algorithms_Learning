72237d32115e0ed337fa01ccaa7fb7b9f12b02f6
Roychenlei <roychenfly@gmail.com>
2017-07-16 11:18:11 +0800

    New Fib

--- Python_algorithms/permutation.py
+++ Python_algorithms/permutation.py
@@ -1,18 +1,24 @@
 #!/user/bin/env python
 #coding: 'utf-8'
-
+import os,sys
 perm_lst = []
 
 def permutation(lst):
 	if  lst == []:
-		print perm_lst
+		print "result:",perm_lst
 	else:
 		for elem in lst:
 			perm_lst.append(elem)
 			rest_lst = lst[:]
+			print "================="
+			print "perm_list1",perm_lst
+			print "rest_list:",rest_lst
+			print "================="
 			rest_lst.remove(elem)
 			permutation(rest_lst)
+			sleep(1000)
 			perm_lst.pop()
+			print "perm_list2",perm_lst
 
 if __name__ == '__main__':
 	permutation(list('abc'))
